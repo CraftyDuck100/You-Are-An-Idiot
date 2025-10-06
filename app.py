f46013e8065b70ff8d98e8764ee0446ec0041694
CraftyDuck100 <61465936+CraftyDuck100@users.noreply.github.com>
2025-10-06 08:50:58 -0500

    fix

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def home():
     if request.method == 'POST':
         name = request.form.get('like', '').strip()
         if not name.lower() == "yes":
-            return render_template("index2.html", hi="good")
-        else:
             return render_template("index2.html", hi="idiot")
+        else:
+            return render_template("index2.html", hi="good")
     return render_template("index.html")
